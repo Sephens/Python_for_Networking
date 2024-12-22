0da1a09c1cd052433240fa1747f3b03582e0f70f
Sephens <adesephens@gmail.com>
2024-12-22 21:00:10 +0300

    22122024

---
+++ get_remote_machine_ip.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+import socket
+def get_remote_machine_info():
+    remote_host = 'www.google.com'
+    try:
+        print ("IP address of %s: %s" %(remote_host,
+        socket.gethostbyname(remote_host)))
+    except socket.error as err_msg:
+        print ("%s: %s" %(remote_host, err_msg))
+if __name__ == '__main__':
+    get_remote_machine_info()
